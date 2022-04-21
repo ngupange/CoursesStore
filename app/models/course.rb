82279a3a7ba9276a33b4f83f2046de8f515adfa5
ngupange <gcab022120@coderacademy.edu.au>
2022-04-21 14:37:54 +1000

    Courses Form Validation

--- app/models/course.rb
+++ app/models/course.rb
@@ -2,4 +2,10 @@ class Course < ApplicationRecord
     has_many :orders
     has_one_attached :cover
     validates :title, presence: true
+    validates :classfication, presence: true
+    validates :language, presence: true
+    validates :status, presence: true
+    validates :instructor, presence: true
+    validates :description, presence: true
+    validates :cover, presence: true
 end
